3e5fe9f0feed458ad67288ba73a317803cec5cf9
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-10-11 22:38:09 -0500

    Starter code for Weather Dashboard

---
+++ script.js
@@ -0,0 +1,5 @@
+const API_KEY = '15fc2a8d6a6a903d2358df970e567b70';
+const API_URL = 'api.openweathermap.org/data/2.5/forecast?lat=35&lon=145&appid=15fc2a8d6a6a903d2358df970e567b70';
+const cityInput = document.getElementById('cityInput');
+
+
